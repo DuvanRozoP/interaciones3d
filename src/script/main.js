af32c54aa5a2f5be2da7d7842c4597cbeb5b6ca6
DuvanRGO <rozoparraduvan@gmail.com>
2022-09-17 15:10:17 -0500

    animacion

--- src/script/main.js
+++ src/script/main.js
@@ -1,40 +1,56 @@
 // OPERACIONES
 
-
-
-// ANIMACIONES 
-function moverresorte(){
-    var tamano = 100; // los pixeles
-    const resorte = document.getElementById('resorte');
-
-    // funcion , milisegundos
-    var repetidorAnimation = setInterval(animation,16);
-    function animation(){
-        if (tamano > 800){
-            clearInterval(repetidorAnimation);
-        }
-
-        resorte.style.width = tamano + "px";
-        tamano++;
-    }
-}
 //la posision y eso
 var amplitud=10
 var gamma=-0.01
 var omega=2
 
 
+
 function obtenerpocion(t){
-
     return amplitud*Math.exp(gamma*t)*Math.cos(omega*t)
-
 }
-for(var t=0; t<100; t+=0.1){
-    
- console.log(obtenerpocion(t))
-    
+
+function masaResorte() {
+    var position= [];
+    for(var t=0; t<100; t += 0.1){
+        position.push(obtenerpocion(t).toFixed(2));
+    }
+    //console.log(position);
+    moverresorte(position)
 }
 //end
 function segundoAMilisegundos(segundos){
     return segundos
+}
+
+// ANIMACIONES 
+function moverresorte(position){
+    console.log(position);
+    var tamano = 200; // los pixeles
+    const resorte = document.getElementById('resorte');
+
+    var repetidorAnimation = setInterval(animation,10);
+    var index = 0;
+
+    function animation(){  
+        if (position[index] == null) {
+            clearInterval(repetidorAnimation);
+        }
+
+        if(position[index] > 0){
+            resorte.style.width = tamano + "px";
+            tamano+= parseFloat(position[index]) + 10 ;
+        }
+
+
+
+
+        if (position[index] < 0) {
+            resorte.style.width = tamano + "px";
+            tamano-= parseFloat(position[index]) - 10;
+        }
+
+        index++;
+    }
 }
