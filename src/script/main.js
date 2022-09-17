59f8fd273368138d16e7da477e24d21ff701b468
macrolomania <113312174+macrolomania@users.noreply.github.com>
2022-09-17 13:03:03 -0500

    la prubea mal

--- src/script/main.js
+++ src/script/main.js
@@ -18,8 +18,21 @@ function moverresorte(){
         tamano++;
     }
 }
-
-
+//la posision y eso
+var amplitud=1
+var gamma=1
+var omega=2
+var t
+var posision
+function obtenerpocion(t){
+    posision=amplitud*Math.exp(gamma*t)*Math.cos(omega*t)
+}
+for(t=0;t<10;t++){
+    
+obtenerpocion()
+    console.log(posision)
+}
+//end
 function segundoAMilisegundos(segundos){
     return segundos
 }
