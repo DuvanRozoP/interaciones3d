e855db57e882959f126b1739b09243381425c20e
DuvanRGO <rozoparraduvan@gmail.com>
2022-09-20 17:27:37 -0500

    osciliacion

--- src/script/animation.mjs
+++ src/script/animation.mjs
@@ -4,22 +4,25 @@ const obtenerpocion = (t,amplitud,gamma,omega) => {
   return amplitud * Math.exp(gamma * t) * Math.cos(omega * t);
 }
 
-const masaResorte = (idElement,amplitud,gamma,omega) => {
-  //100 t
-  for (var t = 0; t < 50; t += 0.1) {
-    var tempPosition = 200 + 3 * parseFloat(obtenerpocion(t,amplitud,gamma,omega).toFixed(2)) + "px"; // 3
-    position.push(tempPosition);
-  }
+export const masaResorte = (idElement,amplitud,gamma,omega) => {
+  
+  //INICIALIZANDO PROMESA
+  return new Promise( (resolve,reject) =>{
+    //100 t
+    for (var t = 0; t < 50; t += 0.2) {
+      var tempPosition = 200 + 3 * parseFloat(obtenerpocion(t,amplitud,gamma,omega).toFixed(2)) + "px"; // 3
+      position.push(tempPosition);
+    }
 
-  // const elementos = ".resorte";
-  const elementos = idElement;
-  console.log(position);
-  anime({
-    targets: elementos,
-    width: position, // -> from '28px' to '100%',
-    duration: 6000, // 1000
-    easing: 'easeInOutQuad',
-  });
+    // const elementos = ".resorte";
+    const elementos = idElement;
+    console.log(position);
+    anime({
+      targets: elementos,
+      width: position, // -> from '28px' to '100%',
+      duration: 6000, // 1000
+      easing: 'easeInOutQuad',
+    });
+  })
 }
 
-module.exports = { masaResorte };
--- src/script/main.mjs
+++ src/script/main.mjs
@@ -4,5 +4,9 @@ const elementoBoton = document.querySelector('.start');
 
 elementoBoton.addEventListener('click', () => {
     console.log('oprimiendo boton')
-    masaResorte(".resorte",20,-0.1,2);
+    masaResorte(".resorte",20,-0.1,2).then(resolve => {
+        console.log("quest complete masa resorte: ",resolve);
+    }).catch(reject => {
+        console.log("quest incomplete masa resorte: ",reject);
+    })
 })
